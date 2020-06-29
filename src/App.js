2f40c0d1ef3281825863fe5f19897cb67df4892a
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2020-06-29 17:15:16 -0400

    added sign-in feature to the front end

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Homepage from './components/pages/homepage/homepage.component';
 import Header from './components/header/header.component';
 import ShopPage from './components/pages/shop/shop.component';
+import SignInAndSignOutPage from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signIn' component={SignInAndSignOutPage} />
       </Switch>
     </div>
   );
