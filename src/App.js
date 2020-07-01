7d3faf334c2b603c36ce2859694aa64e0867b9b3
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2020-07-01 17:14:47 -0400

    added create user document function to verify if the users exists or not when signing up with google

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Homepage from './components/pages/homepage/homepage.component';
 import Header from './components/header/header.component';
 import ShopPage from './components/pages/shop/shop.component';
 import SignInAndSignOutPage from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends Component {
   constructor() {
@@ -21,8 +21,19 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else this.setState({ currentUser: userAuth });
     });
   }
 
